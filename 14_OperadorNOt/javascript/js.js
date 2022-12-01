8d7d801e3a3975d2433ea02eab8ba1c3b51f274b
Vitor <vitormwxm@gmail.com>
2022-12-01 20:40:10 -0300

    Arquivo Operador Lógico NOT

---
+++ 14_OperadorNOt/javascript/js.js
@@ -0,0 +1,14 @@
+if(!false) {
+    console.log ("passou");
+}
+
+var nome = "Matheus"
+
+if (!(nome == "Matheus")) {
+    console.log("o nome é Vitor")
+} else {
+    console.log("o nome é Matheus")
+}
+
+// O Operador Lógico NOT representado pela ! muda o valor que a Expressão retornou
+// se Recebeu True Vira False , se Recebeu False Vira True.
