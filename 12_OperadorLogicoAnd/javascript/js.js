8cfe9054c0c5a6556dfe70adb6c12954df8e9b67
Vitor <vitormwxm@gmail.com>
2022-12-01 19:47:23 -0300

    Arquivo Operador Lógico And

---
+++ 12_OperadorLogicoAnd/javascript/js.js
@@ -0,0 +1,12 @@
+var idade = 18;
+var temDinheiro = true;
+
+
+if (idade >= 18 && temDinheiro === true) {
+    console.log ("pode comprar biscoito");
+} else {
+    console.log ("não pode comprar biscoito");
+}
+
+/* o operador lógico && também conhecido como And , ele vai retornar true apenas se as duas expressões
+retornarem true, qualquer outro resultado do operador lógico And retornará false. */
