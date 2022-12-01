551e24e57fb81d5a7b294f6f1035a1c4c838b0cc
Vitor <vitormwxm@gmail.com>
2022-12-01 20:06:14 -0300

    Arquivo Operador Lógico OR(ou)

---
+++ 12_OperadorLogicoOr/javascript/js.js
@@ -0,0 +1,12 @@
+var idade = 17
+var estarAcompanhado = true
+
+
+if (idade >=18 || estarAcompanhado === true) {
+    console.log("pode subir no brinquedo")
+} else {
+    console.log("não pode brincar")
+}
+
+//o operador lógico || é conhecido também como OR(ou), ele retorna true caso uma das operações
+// retorna verdadeiro , o OR retorna false somente se as duas expressões estiverem falsas.
