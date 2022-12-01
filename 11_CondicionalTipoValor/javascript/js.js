9811054e2805af03142d3ef5d6c4cacb10d94679
Vitor <vitormwxm@gmail.com>
2022-12-01 19:26:48 -0300

    Arquivo Condicional Tipo e Valor

---
+++ 11_CondicionalTipoValor/javascript/js.js
@@ -0,0 +1,11 @@
+var numero = 5 
+
+if (numero === 5) {
+    console.log("o valor é 5")
+} //nessa condicional estamos verificando o tipo e o valor utilizando o sinal de ===
+//ou seja estamos perguntando se é um número e se o valor é 5.
+
+if (numero !== 4) {
+    console.log ('o número não é 5 ')
+} //nessa condicional verificamos o tipo e o valor, se numero é diferente de 4 ou se 
+//nao é um numero , então o código será executado
