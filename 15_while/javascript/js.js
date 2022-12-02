99d6272ebf05e743d31fe126d358b2ed87615cb5
Vitor <vitormwxm@gmail.com>
2022-12-01 21:08:25 -0300

    Arquivo estrutura de Repetição While

---
+++ 15_while/javascript/js.js
@@ -0,0 +1,31 @@
+// abaixo temos o uso do while para pecorrer um Array Estados
+var estados = ['Pernambuco', 'são paulo' , 'MatoGrosso'];
+var i = 0;
+
+while (i <= 2 ) {
+    
+    console.log(estados[i])
+    i++
+}
+
+
+
+// whike é a estrutura de repetição mais simples do JavaScript 
+// a ideia é que se repita algo até atingir tal condição.
+
+
+
+
+
+// abaixo temos o uso do While onde uma particularidade do Js é mostrada , conseguindo percorrer 
+// cada letra de uma Palavra
+
+var nome = "Vitor"
+var y = 0;
+
+while (y <= 5) {
+
+    console.log(nome[y])
+
+    y++
+}
