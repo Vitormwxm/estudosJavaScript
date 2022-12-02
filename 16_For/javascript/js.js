cd7fd3e41be732a1f5fc59a429dc7a89391727b3
Vitor <vitormwxm@gmail.com>
2022-12-01 21:37:19 -0300

    Arquivo Estrutura For

---
+++ 16_For/javascript/js.js
@@ -0,0 +1,8 @@
+var arr = ["Vitor" , "karol" , "Clara" , "Guilherme" , "Angela"]
+
+for ( var i = 0 ; i < arr.length; i++) {
+    console.log(arr[i])
+}
+
+// O For tem uma Sintaxe mais complexa, mas é optada pela maioria dos programadores, apesar
+// de parecer mais difícil, como a instrução fica toda em uma linha propociona mais controle.
