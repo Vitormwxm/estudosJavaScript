018093af307d476b2c5c183fa7b0f4235a444548
Vitor <vitormwxm@gmail.com>
2022-12-06 22:12:07 -0300

    arquivo  break e continue

---
+++ 17_Break_and_Continue/javascript/js.js
@@ -0,0 +1,25 @@
+
+
+for(i = 10; i <= 10; i--){
+    console.log(i)
+    if( i === -10){
+        console.log(i)
+        break
+    }
+}
+
+x = 10
+
+while( x < 100) {
+    console.log(x)
+    x++ 
+
+    if(x === 40 ){
+        console.log("continue");
+        continue;
+    } 
+    
+    
+    
+
+}
